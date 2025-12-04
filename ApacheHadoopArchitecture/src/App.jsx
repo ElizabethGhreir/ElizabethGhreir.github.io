8d5c9ae10d4db68f1e861cc15c9f46b94f878277
Ayan Saiyed <ayan.unit8@gmail.com>
2025-12-03 23:32:36 -0500

    uploading deliverable 3 report

--- ApacheHadoopArchitecture/src/App.jsx
+++ ApacheHadoopArchitecture/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
 
       <section className="group">
         <h2>Group A</h2>
-        <p><strong>Members:</strong></p>
+        <p>
+          <strong>Members:</strong>
+        </p>
         <ul>
           <li>Ayan Saiyed - 219524602</li>
           <li>Elizabeth Ghreir - 218093443</li>
@@ -16,25 +18,48 @@ function App() {
           <li>Mhd Ryiad Almohamad Alelaioy - 219894781</li>
         </ul>
       </section>
-      
+
       <section className="deliverable">
         <h2>Deliverable 1</h2>
-        <p><a href="https://docs.google.com/presentation/d/1qRyzY7-mWz3ZqF0fqglUA3hc0tuYwHXMAR4Rq3E8VkU/edit?usp=sharing">Presentation</a></p>
-        <p><a href="https://docs.google.com/document/d/1sGKfXk1klGfpqvDgLzSJW4Gk3N1Fr7UCtZbwP_zTaFw/edit?usp=sharing">Report</a></p>
+        <p>
+          <a href="https://docs.google.com/presentation/d/1qRyzY7-mWz3ZqF0fqglUA3hc0tuYwHXMAR4Rq3E8VkU/edit?usp=sharing">
+            Presentation
+          </a>
+        </p>
+        <p>
+          <a href="https://docs.google.com/document/d/1sGKfXk1klGfpqvDgLzSJW4Gk3N1Fr7UCtZbwP_zTaFw/edit?usp=sharing">
+            Report
+          </a>
+        </p>
       </section>
 
       <section className="deliverable">
         <h2>Deliverable 2</h2>
-        <p><a href="https://docs.google.com/presentation/d/1pAlj7erTIxniFAdIzVKcv9YkdhjQ8Xs1VAJI5Z6p0fM/edit?usp=sharing">Presentation</a></p>
-        <p><a href="https://docs.google.com/document/d/1wsJkjCcLR1XJ7nvdpZjAulAsMYaNqOsecZbqSdVnLVE/edit?usp=sharing">Report</a></p>
+        <p>
+          <a href="https://docs.google.com/presentation/d/1pAlj7erTIxniFAdIzVKcv9YkdhjQ8Xs1VAJI5Z6p0fM/edit?usp=sharing">
+            Presentation
+          </a>
+        </p>
+        <p>
+          <a href="https://docs.google.com/document/d/1wsJkjCcLR1XJ7nvdpZjAulAsMYaNqOsecZbqSdVnLVE/edit?usp=sharing">
+            Report
+          </a>
+        </p>
       </section>
 
       <section className="deliverable">
         <h2>Deliverable 3</h2>
-        <p><a href="https://docs.google.com/presentation/d/1pl3SGtqPLLoZdZp3ejF8NByK1S_k5vW4wZ5OC7wXtMc/edit?usp=sharing">Presentation</a></p>
-        <p><a href="#">Report</a></p>
+        <p>
+          <a href="https://docs.google.com/presentation/d/1pl3SGtqPLLoZdZp3ejF8NByK1S_k5vW4wZ5OC7wXtMc/edit?usp=sharing">
+            Presentation
+          </a>
+        </p>
+        <p>
+          <a href="https://docs.google.com/document/d/1hsEBufcmAJZ3N6ZHIeT5weuTdX42m51iUE9RVbVUvXw/edit?usp=sharing">
+            Report
+          </a>
+        </p>
       </section>
-            
     </main>
   );
 }
